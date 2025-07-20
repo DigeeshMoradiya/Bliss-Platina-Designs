297ca6850be7eaa7f0a5d1fe0f649ada1f585ae2
Digeesh Moradiya <digeeshmordiya@gmail.com>
2025-07-20 19:55:10 +0530

    Changes by digeesh

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ export default function Custom404() {
     <div className="min-h-screen flex flex-col items-center justify-center text-center bg-gray-100 p-6 section-padding">
       <h1 className="text-6xl font-bold text-red-500">404</h1>
       <h2 className="text-2xl mt-4 text-gray-700">Page Not Found</h2>
-      <p className="mt-2 text-gray-500">The page you're looking for doesn't exist.</p>
+      <p className="mt-2 text-gray-500">The page you&rsquo;re looking for doesn&rsquo;t exist.</p>
          <Link href="/" className="btn btn-hero"> Go back home</Link>
     </div>
   );
