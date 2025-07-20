f966a4540beae2b5b8276281a8f89a5db774b531
Digeesh Moradiya <digeeshmordiya@gmail.com>
2025-07-20 19:57:27 +0530

    Changes by digeesh

--- src/pages/test-error-client.js
+++
@@ -1,9 +0,0 @@
-// pages/test-error-client.js
-
-export default function ClientErrorPage() {
-  if (typeof window === 'undefined') {
-    throw new Error('Server-side render error'); // Will trigger 500 page
-  }
-
-  return <div>This renders only on the client</div>;
-}
