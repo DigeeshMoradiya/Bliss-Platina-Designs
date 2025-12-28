1352eb1459e37fc004c7822c4c351a9ad2e8f5ce
Digeesh Moradiya <digeeshmordiya@gmail.com>
2025-12-28 11:39:35 +0530

    change

--- src/components/common/WhatsAppButton.js
+++ src/components/common/WhatsAppButton.js
@@ -1,13 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const WhatsAppButton = ({ phone }) => {
+    const [moveUp, setMoveUp] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 300) {
+                setMoveUp(true);
+            } else {
+                setMoveUp(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     if (!phone) return null;
 
     // Remove any non-numeric characters from the phone number
     const formattedPhone = phone.replace(/\D/g, '');
 
     return (
-        <div className="whatsapp-button">
+        <div className={`whatsapp-button ${moveUp ? 'move-up' : ''}`}>
             <Link
                 href={`https://wa.me/${formattedPhone}`}
                 target="_blank"
